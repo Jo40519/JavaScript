38a911bbcbafbbb0aeb050dac9b902966fbd4441
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2021-09-28 00:00:56 -0300

    if e else
    Aprendendo sobre if e else. Aprendendo usar console.log. Condições simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade so seu carro é de ${vel}km/h`);
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!`)
+}
+console.log('Dirija sempre usando o cinto de segurança.');
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Morando em ${país}`);
+if (país == Brasil){
+    console.log(`Brsaileiro`);
+} else{
+    console.log(`Estrangeiro.`);
+}
